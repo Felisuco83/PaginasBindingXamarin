e9693bcdb4700aba22712cc5725604f20a2a7447
Felix Itiel Arino Gila <FelixItiel.Arino@tajamar365.com>
2021-05-19 14:22:53 +0200

    tete

---
+++ PaginasBinding/PaginasBinding/PaginasBinding/PaginasBinding/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PaginasBinding.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PaginasBinding
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new BindingClassView();
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PaginasBinding/PaginasBinding/PaginasBinding/PaginasBinding/Views/BindingClassView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PaginasBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PaginasBinding.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BindingClassView : ContentPage
+    {
+        public BindingClassView()
+        {
+            InitializeComponent();
+            Coche car = new Coche();
+            car.Marca = "VW";
+            car.Modelo = "Golf";
+            car.Velocidad = 280;
+            car.Imagen = "https://images1.autocasion.com/unsafe/1200x800/actualidad/wp-content/uploads/2020/10/volkswagen-golf-gti-clubsport-1.jpg";
+            this.BindingContext = car;
+        }
+    }
+}
